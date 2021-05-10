def58cbc4a5a4c0141edcabde8dc2fe22a5edcfb
Andrew Law <u6673406@anu.edu.au>
2021-05-10 21:47:42 +1000

    comments

--- tests/testImageService.js
+++ tests/testImageService.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const path = require('path');
 const app = require('../app');
 
+//TODO: Fix integration tests
 describe('POST /api/upload', function () {
     console.log(__dirname)
     it('responds with json', function(done) {
